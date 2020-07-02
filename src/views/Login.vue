b389ed92b0167690fbccd1d866caafe7b235c9f6
Kasen IO <kasenvr@gmail.com>
2020-07-02 15:08:29 -0400

    Second commit

---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<!--
+//  Login.vue
+//
+//  Created by Kalila L. on 20 June 2020.
+//  Copyright 2020 Vircadia and contributors.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+-->
+
+<template>
+  <div class="login">
+    TEST
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Login',
+    components: {
+        
+    }
+}
+</script>
