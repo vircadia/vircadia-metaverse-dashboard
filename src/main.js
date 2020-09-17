f68062d1fd1b9248f98ae9b65b26858c69af5f21
Kalila L <kasenvr@gmail.com>
2020-09-17 03:28:10 -0400

    Update copyright headers.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 //  main.js
 //
 //  Created by Kalila L. on 2 Jul 2020.
-//  Copyright 2020 Vircadia and contributors.
+//  Copyright 2020 Vircadia contributors.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
--- src/plugins/eventBus.js
+++ src/plugins/eventBus.js
@@ -2,7 +2,7 @@
     eventBus.js
 
     Created by Kalila L. on 6 Jul 2020.
-    Copyright 2020 Vircadia and contributors.
+    Copyright 2020 Vircadia contributors.
 
     Distributed under the Apache License, Version 2.0.
     See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
