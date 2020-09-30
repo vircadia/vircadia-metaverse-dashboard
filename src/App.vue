f3ee49a77b51eb3fd0de73ab293d387b8f2fa7c0
Kalila L <kasenvr@gmail.com>
2020-09-29 22:30:50 -0400

    Bump to version 0.0.1j

--- src/App.vue
+++ src/App.vue
@@ -101,7 +101,7 @@
             color="primary"
             app
         >
-            <span class="white--text">Iamus Dashboard v0.0.1i | Vircadia | <a class="white--text" href="https://github.com/vircadia/project-iamus-dashboard/issues">Feedback</a></span>
+            <span class="white--text">Iamus Dashboard v0.0.1j | Vircadia | <a class="white--text" href="https://github.com/vircadia/project-iamus-dashboard/issues">Feedback</a></span>
             <v-spacer></v-spacer>
             <span class="white--text">Metaverse: <b>{{ $store.state.metaverseConfig.server }}</b></span>
             <span v-show="$store.state.metaverseConfig.serverVersion" class="white--text ml-4">Version: {{ $store.state.metaverseConfig.serverVersion["npm-package-version"] }}</span>
